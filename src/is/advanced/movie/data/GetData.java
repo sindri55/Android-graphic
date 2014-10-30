8a83547017fc4ace8023fadf317e3cb6204e3fc2
alexanderbsig <alexnaderbs.js@gmail.com>
2014-10-30 16:08:15 +0000

    Now we are not reading Json in main thread

--- src/is/advanced/movie/data/GetData.java
+++ src/is/advanced/movie/data/GetData.java
@@ -17,7 +17,7 @@ import is.advanced.movie.models.Showtime;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class GetData  extends AsyncTask<String, String, String> {
+public class GetData  extends AsyncTask<String, String, List<Movie>> {
 
     public List<Movie> movieList = new ArrayList<Movie>();
 
@@ -29,7 +29,7 @@ public class GetData  extends AsyncTask<String, String, String> {
        this.handler = h;
     }
     @Override
-    protected String doInBackground(String... uri) {
+    protected List<Movie> doInBackground(String... uri) {
 
         String inputLine;
         StringBuilder response = new StringBuilder();
@@ -53,62 +53,60 @@ public class GetData  extends AsyncTask<String, String, String> {
             System.out.println("Could not get data ");
         }
 
-        return response.toString();
+        try{
+
+            JSONObject json = new JSONObject(response.toString());
+            JSONArray j = json.getJSONArray("results");
+
+            for(int i = 0; i < j.length(); i++)
+            {
+                JSONObject movies = j.getJSONObject(i);
+                String title = (String) movies.get("title");
+                String released = (String) movies.get("released");
+                String restricted = (String) movies.get("restricted");
+                String imdb = (String) movies.get("imdb");
+                String image = (String) movies.get("image");
+
+                JSONArray showtimes = movies.getJSONArray("showtimes");
+
+                List<Showtime> showtimeList = new ArrayList<Showtime>();
+
+                for(int y = 0; y < showtimes.length(); y++)
+                {
+
+                    JSONObject showtime = showtimes.getJSONObject(y);
+                    String theater = (String) showtime.get("theater");
+
+                    List<String> scheduleList = new ArrayList<String>();
+
+                    JSONArray scheduleArr = showtime.getJSONArray("schedule");
+
+                    for(int x = 0; x < scheduleArr.length(); x++)
+                    {
+                        scheduleList.add(scheduleArr.getString(x));
+                    }
+
+                    Showtime showtimeForMovie = new Showtime(theater,scheduleList);
+                    showtimeList.add(showtimeForMovie);
+
+                }
+
+                Movie movie = new Movie(title,released,restricted,imdb,image,showtimeList);
+                movieList.add(movie);
+
+            }
+
+        }
+        catch(Exception e){
+            System.out.println("Error reading Json");
+        }
+        return movieList;
     }
 
     @Override
-    protected void onPostExecute(String result) {
+    protected void onPostExecute(List<Movie> result) {
         super.onPostExecute(result);
 
-        try {
-            JSONObject json = new JSONObject(result);
-            JSONArray j = json.getJSONArray("results");
-
-        for(int i = 0; i < j.length(); i++)
-        {
-            JSONObject movies = j.getJSONObject(i);
-            String title = (String) movies.get("title");
-            String released = (String) movies.get("released");
-            String restricted = (String) movies.get("restricted");
-            String imdb = (String) movies.get("imdb");
-            String image = (String) movies.get("image");
-
-            JSONArray showtimes = movies.getJSONArray("showtimes");
-
-            List<Showtime> showtimeList = new ArrayList<Showtime>();
-
-            for(int y = 0; y < showtimes.length(); y++)
-            {
-
-                JSONObject showtime = showtimes.getJSONObject(y);
-                String theater = (String) showtime.get("theater");
-
-                List<String> scheduleList = new ArrayList<String>();
-
-                JSONArray scheduleArr = showtime.getJSONArray("schedule");
-
-                for(int x = 0; x < scheduleArr.length(); x++)
-                {
-                    scheduleList.add(scheduleArr.getString(x));
-                }
-
-                Showtime showtimeForMovie = new Showtime(theater,scheduleList);
-                showtimeList.add(showtimeForMovie);
-
-            }
-
-            Movie movie = new Movie(title,released,restricted,imdb,image,showtimeList);
-            movieList.add(movie);
-
-            }
-        }
-        catch (Exception e) {
-            System.out.println("Error reading json");
-        }
-
-        for(Movie m : movieList){
-            System.out.println(m.getTitle());
-        }
         Global.getInstance().setMovieList(movieList);
         handler.sendEmptyMessage(42);
     }
